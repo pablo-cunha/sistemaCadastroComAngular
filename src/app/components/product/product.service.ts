d3b1a3072d8d0e298e29408973d507a918580c91
Pablo Cunha <p.abloc1@hotmail.com>
2021-09-27 16:01:40 -0300

    Primeiro Commit

---
+++ src/app/components/product/product.service.ts
@@ -0,0 +1,59 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar'
+import { EMPTY, Observable } from 'rxjs';
+import { Product } from './product.model';
+
+import { catchError, map } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductService {
+
+  baseUrl = "http://localhost:3001/products"
+
+  constructor(private snackbar: MatSnackBar, private http: HttpClient) { }
+
+  showMessage(msg: string, isError: boolean = false): void {
+    this.snackbar.open(msg, 'X', {
+      duration: 3000,
+      horizontalPosition: "right",
+      verticalPosition: "top",
+      panelClass: isError ? ['msg-error'] : ['msg-success']
+    })
+  }
+
+  create(product: Product): Observable<Product> {
+    return this.http.post<Product>(this.baseUrl, product).pipe(
+      map((obj) => obj),
+      catchError(e => this.errorHandler(e))
+    )
+  }
+
+  errorHandler(e: any): Observable<any> {
+    this.showMessage('Opa! Houve um erro :(', true)
+    return EMPTY
+  }
+
+  read(): Observable<Product[]>  {
+    return this.http.get<Product[]>(this.baseUrl)
+  }
+
+  readById(id: any): Observable<Product> {
+      const url = `${this.baseUrl}/${id}`
+      return this.http.get<Product>(url)
+  }
+
+  update(product: Product): Observable<Product> {
+    const url = `${this.baseUrl}/${product.id}`
+    return this.http.put<Product>(url, product)
+  }
+
+  delete(id: string): Observable<Product> {
+    const url = `${this.baseUrl}/${id}`
+    return this.http.delete<Product>(url)
+  }
+
+  }
+ 
